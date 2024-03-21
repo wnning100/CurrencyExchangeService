9966e7e9bbaa4494411be5a2cf731facb4f680e0
wenningyang <tur860705@gmail.com>
2024-03-22 03:54:11 +0800

    [新增] 匯率轉換 Function

--- Service/CurrencyExchangeService.cs
+++ Service/CurrencyExchangeService.cs
@@ -8,5 +8,26 @@
             _exchangeRate = exchangeRate ?? throw new ArgumentNullException(nameof(exchangeRate));
         }
 
+        /// <summary>
+        /// 匯率轉換
+        /// </summary>
+        /// <param name="source">輸入幣別</param>
+        /// <param name="target">輸出幣別</param>
+        /// <param name="amount">匯率</param>
+        /// <returns></returns>
+        public string ConvertCurrency(string source, string target, decimal amount)
+        {
+            if (_exchangeRate.ContainsKey(source) && _exchangeRate[source].ContainsKey(target))
+            {
+                var targetAmount = amount * _exchangeRate[source][target];
+                // 四捨五入取小數點到第二位
+                targetAmount = Math.Round(targetAmount, 2);
+                // 將結果轉換為字串，並加入千分位
+                var result = string.Format("{0:N}", targetAmount);
+                return result;
+            }
+
+            return "Invalid Currency.";
+        }
     }
 }
